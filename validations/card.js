e4c7601296829e8a158026af11aef5938310d415
bababum <bababum1995@gmail.com>
2023-07-02 20:04:23 +0700

    validate link

--- validations/card.js
+++ validations/card.js
@@ -3,6 +3,6 @@ const { celebrate, Joi } = require('celebrate');
 module.exports.validateCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(/(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/),
   }),
 });
--- validations/user.js
+++ validations/user.js
@@ -1,10 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
 
+const urlPattern = /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/;
+
 module.exports.validateCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(urlPattern),
     email: Joi.string().required(),
     password: Joi.string().required(),
   }),
@@ -26,6 +28,6 @@ module.exports.validateUpdateUserInfo = celebrate({
 
 module.exports.validateUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required(),
+    avatar: Joi.string().required().pattern(urlPattern),
   }),
 });
