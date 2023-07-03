deaa5db9bb17247d5afad48240d33e21737946b5
bababum <bababum1995@gmail.com>
2023-07-03 13:57:53 +0700

    hotfix

--- app.js
+++ app.js
@@ -29,11 +29,10 @@ app.use((err, req, res, next) => {
   if (err.code === 11000) {
     res.status(409).send({ message: 'Пользователь с таким email уже существует' });
   } else {
-    // const { statusCode = 500, message } = err;
-    // res.status(statusCode).send({
-    //   message: statusCode === 500 ? 'Внутренняя ошибка сервера' : message,
-    // });
-    res.send({ message: err.message, name: err.name });
+    const { statusCode = 500, message } = err;
+    res.status(statusCode).send({
+      message: statusCode === 500 ? 'Внутренняя ошибка сервера' : message,
+    });
   }
 });
 
